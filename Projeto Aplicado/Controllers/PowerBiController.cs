26d8da59b9018822b4d59608a21476ec8fbd78a2
Leonardo Vanni Bonavigo <leonardovanni21@gmail.com>
2022-11-08 13:26:13 -0300

    ajustando tela projetos4 e powerbi

---
+++ Projeto Aplicado/Controllers/PowerBiController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Projeto_Aplicado.Entidades;
+using Projeto_Aplicado.Models;
+using System;
+
+namespace Projeto_Aplicado.Controllers
+{
+    public class PowerBiController : Controller
+    {
+        public IActionResult PowerBi()
+        {
+            try
+            {
+                var sessionUser = JsonConvert.DeserializeObject<Usuario>(HttpContext?.Session.GetString("SessionUser"));
+            }
+            catch (Exception e)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            var model = new PowerBiModel();
+            return View(model);
+        }
+    }
+}
